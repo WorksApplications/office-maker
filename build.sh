022f4fec242bdbeced0c35f3bfb2f8f472cdddfd
Yosuke Torii <jinjorweb@gmail.com>
2016-04-22 23:20:22 +0900

    use webfont from CDN

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 elm-make src/elm/Main.elm --output=index.js --warn
 mkdir -p test/public
-cp -f index.js src/index.html src/style.css lib/Roboto-Light.ttf test/public
+cp -f index.js src/index.html src/style.css test/public
