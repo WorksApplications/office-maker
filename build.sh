7af0da12fe117edfa286a9fd3a4ca9f236d090e8
Yosuke Torii <jinjorweb@gmail.com>
2016-04-29 21:19:33 +0900

    fixed build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-elm-make src/elm/Main.elm --output=index.js --warn
-mkdir -p test/public
+elm-make src/elm/Main.elm --output=index.js --warn &&
+mkdir -p test/public &&
 cp -f index.js src/index.html src/style.css test/public
