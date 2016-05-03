7125bde3770ba0006612ff68bdb4f81d8e43fb6b
Yosuke Torii <jinjorweb@gmail.com>
2016-05-03 21:15:51 +0900

    fix login

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 elm-make src/elm/Main.elm --output=index.js --warn &&
-# elm-make src/elm/Login.elm --output=login.js --warn &&
-# elm-make src/elm/BugReport.elm --output=bugs.html --warn &&
+elm-make src/elm/Login.elm --output=login.js --warn &&
 mkdir -p test/public &&
 cp -f index.js src/index.html login.js src/login.html src/style.css test/public
--- src/elm/BugReport.elm
+++
@@ -1,9 +0,0 @@
-import Html exposing (Html, text)
-import Json.Decode as Json exposing (Decoder)
-
-main : Html
-main = text <| toString <| Json.decodeString decoder ""
-
-decoder : Decoder String
-decoder =
-  Json.succeed "Succeed!"
