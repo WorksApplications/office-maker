81fdd88661e37b3848330635a23df9889737302f
torii_y <torii_y@worksap.co.jp>
2016-08-23 16:12:10 +0900

    scale more

--- src/elm/Model/Scale.elm
+++ src/elm/Model/Scale.elm
@@ -1,15 +1,19 @@
 module Model.Scale exposing (..)
 
+
 type Action = ScaleUp | ScaleDown
+
 
 type alias Model =
   { scaleDown : Int
   }
 
+
 init : Model
 init =
   { scaleDown = 0
   }
+
 
 update : Action -> Model -> Model
 update action model =
@@ -17,17 +21,20 @@ update action model =
     ScaleUp ->
       { model | scaleDown = max 0 (model.scaleDown - 1) }
     ScaleDown ->
-      { model | scaleDown = min 2 (model.scaleDown + 1) }
+      { model | scaleDown = min 4 (model.scaleDown + 1) }
+
 
 screenToImageForPosition : Model -> (Int, Int) -> (Int, Int)
 screenToImageForPosition model (screenX, screenY) =
   ( screenToImage model screenX
   , screenToImage model screenY)
 
+
 imageToScreenForPosition : Model -> (Int, Int) -> (Int, Int)
 imageToScreenForPosition model (imageX, imageY) =
   ( imageToScreen model imageX
   , imageToScreen model imageY)
+
 
 imageToScreenForRect : Model -> (Int, Int, Int, Int) -> (Int, Int, Int, Int)
 imageToScreenForRect model (x, y, w, h) =
@@ -37,6 +44,7 @@ imageToScreenForRect model (x, y, w, h) =
   , imageToScreen model h
   )
 
+
 screenToImageForRect : Model -> (Int, Int, Int, Int) -> (Int, Int, Int, Int)
 screenToImageForRect model (x, y, w, h) =
   ( screenToImage model x
@@ -45,17 +53,21 @@ screenToImageForRect model (x, y, w, h) =
   , screenToImage model h
   )
 
+
 screenToImage : Model -> Int -> Int
 screenToImage model imageLength =
   imageLength * (2 ^ model.scaleDown)
+
 
 imageToScreen : Model -> Int -> Int
 imageToScreen model screenLength =
   screenLength // (2 ^ model.scaleDown)
 
+
 imageToScreenRatio : Model -> Float
 imageToScreenRatio model =
   1.0 / (2 ^ (toFloat model.scaleDown))
+
 
 ratio : Model -> Model -> Float
 ratio old new =
