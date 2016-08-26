941573538758e148f4d03e7d1d32dbe853d1c108
jinjor <jinjorweb@gmail.com>
2016-08-27 00:27:17 +0900

    fix shell script

--- server/setup-mysql.sh
+++ server/setup-mysql.sh
@@ -1,4 +1,12 @@
 # for developmemt
-mysql -uroot < sql/1-create-schema.sql
-mysql -uroot map2 < sql/2-create-tables.sql
+user=root
+pass=root
+
+cat defaultConfig.json \
+  | sed -e "s/\"user\": \"root\"/\"user\": \"$user\"/"\
+  | sed -e "s/\"pass\": \"\"/\"pass\": \"$pass\"/" \
+  > config.json
+mysql --user=$user --password=$pass -e "drop database map2;"
+mysql --user=$user --password=$pass < sql/1-create-schema.sql
+mysql --user=$user --password=$pass < sql/2-create-tables.sql
 node commands.js createDataForDebug onpremiss
