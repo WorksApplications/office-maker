caa114a14702791286eb4034e519042dbdf25bf2
torii_y <torii_y@worksap.co.jp>
2016-10-20 20:18:35 +0900

    load data to master page

--- src/elm/Page/Master.elm
+++ src/elm/Page/Master.elm
@@ -11,8 +11,15 @@ import Task
 import Http
 
 import API.API as API
+import API.Cache as Cache exposing (Cache, UserState)
 import Component.Header as Header
+
 import Model.I18n as I18n exposing (Language(..))
+import Model.User as User exposing (User)
+import Model.Prototype exposing (Prototype)
+import Model.Prototypes as Prototypes exposing (..)
+import Model.ColorPalette as ColorPalette exposing (ColorPalette)
+
 import View.Styles as Styles
 
 port removeToken : {} -> Cmd msg
@@ -34,7 +41,7 @@ main =
 
 
 type alias Model =
-  { accountServiceRoot : String
+  { apiConfig : API.Config
   , title : String
   , error : Maybe String
   , headerState : Header.State
@@ -56,17 +63,50 @@ type alias Flags =
 
 type Msg
   = NoOp
+  | Loaded UserState User ColorPalette (List Prototype)
   | UpdateHeaderState Header.Msg
+  | NotAuthorized
+  | APIError Http.Error
 
 
 init : Flags -> (Model, Cmd Msg)
-init { apiRoot, accountServiceRoot, authToken, title, lang } =
-  { accountServiceRoot = accountServiceRoot
-  , title = title
-  , error = Nothing
-  , headerState = Header.init
-  , lang = if lang == "ja" then JA else EN
-  } ! []
+init flags =
+  let
+    apiConfig =
+      { apiRoot = flags.apiRoot
+      , accountServiceRoot = flags.accountServiceRoot
+      , token = flags.authToken
+      }
+
+    defaultUserState =
+      Cache.defaultUserState (if flags.lang == "ja" then JA else EN)
+  in
+    { apiConfig = apiConfig
+    , title = flags.title
+    , error = Nothing
+    , headerState = Header.init
+    , lang = if flags.lang == "ja" then JA else EN
+    } ! [ initCmd apiConfig defaultUserState ]
+
+
+initCmd : API.Config -> UserState -> Cmd Msg
+initCmd apiConfig defaultUserState =
+  performAPI
+    identity
+    ( Cache.getWithDefault Cache.cache defaultUserState `Task.andThen` \userState ->
+      API.getAuth apiConfig `Task.andThen` \user ->
+        if User.isGuest user then
+          Task.succeed NotAuthorized
+        else
+          API.getColors apiConfig `Task.andThen` \colors ->
+          API.getPrototypes apiConfig `Task.andThen` \prototypes ->
+          Task.succeed (Loaded userState user colors prototypes)
+    )
+
+
+performAPI : (a -> Msg) -> Task.Task API.Error a -> Cmd Msg
+performAPI tagger task =
+  Task.perform APIError tagger task
 
 
 update : ({} -> Cmd Msg) -> Msg -> Model -> (Model, Cmd Msg)
@@ -75,9 +115,17 @@ update removeToken message model =
     NoOp ->
       model ! []
 
+    Loaded userState user colors prototypes ->
+      model ! [] -- TODO
+
     UpdateHeaderState msg ->
       { model | headerState = Header.update msg model.headerState } ! []
 
+    NotAuthorized ->
+      model ! [ Task.perform (always NoOp) (always NoOp) API.goToLogin ]
+
+    APIError e ->
+      { model | error = Just (toString e) } ! []
 
 
 view : Model -> Html Msg
