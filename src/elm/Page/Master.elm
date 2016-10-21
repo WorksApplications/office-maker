da0d4d0674fc5d18761f98fff9196706790f9fe0
torii_y <torii_y@worksap.co.jp>
2016-10-21 15:32:20 +0900

    proto of color master

--- src/elm/Page/Master.elm
+++ src/elm/Page/Master.elm
@@ -1,14 +1,14 @@
 port module Page.Master exposing (..)
 
+import Time exposing (second)
+import Task
+import Http
 import Navigation
 
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
-import Task
-import Http
 
 import API.API as API
 import API.Cache as Cache exposing (Cache, UserState)
@@ -20,7 +20,11 @@ import Model.Prototype exposing (Prototype)
 import Model.Prototypes as Prototypes exposing (..)
 import Model.ColorPalette as ColorPalette exposing (ColorPalette)
 
+import View.Common exposing (..)
 import View.Styles as Styles
+
+import Debounce exposing (Debounce)
+
 
 port removeToken : {} -> Cmd msg
 
@@ -43,9 +47,11 @@ main =
 type alias Model =
   { apiConfig : API.Config
   , title : String
+  , colors : List String
   , error : Maybe String
   , headerState : Header.State
   , lang : Language
+  , saveColorDebounce : Debounce (List String)
   }
 
 
@@ -65,6 +71,8 @@ type Msg
   = NoOp
   | Loaded UserState User ColorPalette (List Prototype)
   | UpdateHeaderState Header.Msg
+  | InputColor Int String
+  | SaveColorDebounceMsg Debounce.Msg
   | NotAuthorized
   | APIError Http.Error
 
@@ -83,9 +91,11 @@ init flags =
   in
     { apiConfig = apiConfig
     , title = flags.title
+    , colors = ["#afd", "#456", "#f49"]
     , error = Nothing
     , headerState = Header.init
-    , lang = if flags.lang == "ja" then JA else EN
+    , lang = defaultUserState.lang
+    , saveColorDebounce = Debounce.init
     } ! [ initCmd apiConfig defaultUserState ]
 
 
@@ -95,9 +105,9 @@ initCmd apiConfig defaultUserState =
     identity
     ( Cache.getWithDefault Cache.cache defaultUserState `Task.andThen` \userState ->
       API.getAuth apiConfig `Task.andThen` \user ->
-        if User.isGuest user then
-          Task.succeed NotAuthorized
-        else
+        -- if User.isGuest user then
+        --   Task.succeed NotAuthorized
+        -- else
           API.getColors apiConfig `Task.andThen` \colors ->
           API.getPrototypes apiConfig `Task.andThen` \prototypes ->
           Task.succeed (Loaded userState user colors prototypes)
@@ -107,6 +117,13 @@ initCmd apiConfig defaultUserState =
 performAPI : (a -> Msg) -> Task.Task API.Error a -> Cmd Msg
 performAPI tagger task =
   Task.perform APIError tagger task
+
+
+saveColorDebounceConfig : Debounce.Config Msg
+saveColorDebounceConfig =
+  { strategy = Debounce.later (1 * second)
+  , transform = SaveColorDebounceMsg
+  }
 
 
 update : ({} -> Cmd Msg) -> Msg -> Model -> (Model, Cmd Msg)
@@ -121,6 +138,33 @@ update removeToken message model =
     UpdateHeaderState msg ->
       { model | headerState = Header.update msg model.headerState } ! []
 
+    InputColor index color ->
+      let
+        colors =
+          setColor index color model.colors
+
+        (saveColorDebounce, cmd) =
+          Debounce.push
+            saveColorDebounceConfig
+            colors
+            model.saveColorDebounce
+      in
+        { model
+        | colors = colors
+        , saveColorDebounce = saveColorDebounce
+        } ! [ cmd ]
+
+    SaveColorDebounceMsg msg ->
+      let
+        (saveColorDebounce, cmd) =
+          Debounce.update
+            saveColorDebounceConfig
+            (Debounce.takeLast saveColors)
+            msg
+            model.saveColorDebounce
+      in
+        { model | saveColorDebounce = saveColorDebounce } ! [ cmd ]
+
     NotAuthorized ->
       model ! [ Task.perform (always NoOp) (always NoOp) API.goToLogin ]
 
@@ -128,22 +172,63 @@ update removeToken message model =
       { model | error = Just (toString e) } ! []
 
 
+setColor : Int -> String -> List String -> List String
+setColor index color list =
+  case list of
+    head :: tail ->
+      if index == 0 then
+        color :: tail
+      else
+        head :: setColor (index - 1) color tail
+
+    [] ->
+      list
+
+
+saveColors : List String -> Cmd Msg
+saveColors colors = Cmd.none -- TODO
+
+
 view : Model -> Html Msg
 view model =
   div
     []
-    [ Header.view
-        { onSignInClicked = NoOp
-        , onSignOutClicked = NoOp
-        , onToggleEditing = NoOp
-        , onTogglePrintView = NoOp
-        , onSelectLang = \_ -> NoOp
-        , onUpdate = UpdateHeaderState
-        , title = model.title
-        , lang = EN
-        , user = Nothing
-        , editing = False
-        , printMode = False
-        }
-        model.headerState
+    [ headerView model
+    , card <| colorMasterView model
     ]
+
+
+headerView : Model -> Html Msg
+headerView model =
+  Header.view
+    { onSignInClicked = NoOp
+    , onSignOutClicked = NoOp
+    , onToggleEditing = NoOp
+    , onTogglePrintView = NoOp
+    , onSelectLang = \_ -> NoOp
+    , onUpdate = UpdateHeaderState
+    , title = model.title
+    , lang = EN
+    , user = Nothing
+    , editing = False
+    , printMode = False
+    }
+    model.headerState
+
+
+colorMasterView : Model -> List (Html Msg)
+colorMasterView model =
+  List.indexedMap row model.colors
+
+
+row : Int -> String -> Html Msg
+row index color =
+  div [ style [("height", "30px"), ("display", "flex")] ]
+    [ colorSample color
+    , input [ onInput (InputColor index), value color ] []
+    ]
+
+
+colorSample : String -> Html Msg
+colorSample color =
+  div [ style [("background-color", color), ("width", "30px")] ] [ ]
