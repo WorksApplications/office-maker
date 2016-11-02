ae4c691f27fce520d88d52fdb4d113fca29bb386
torii_y <torii_y@worksap.co.jp>
2016-11-02 18:35:03 +0900

    delete unused import

--- src/elm/Page/Master/Model.elm
+++ src/elm/Page/Master/Model.elm
@@ -1,20 +1,12 @@
 module Page.Master.Model exposing (..)
 
-import Time exposing (second)
-import Task
-import Http
-import Navigation
-
 import Component.Header as Header
 
 import Model.I18n as I18n exposing (Language(..))
-import Model.User as User exposing (User)
 import Model.Prototype exposing (Prototype)
-import Model.Prototypes as Prototypes exposing (..)
 import Model.ColorPalette as ColorPalette exposing (ColorPalette)
 
 import API.API as API
-import API.Cache as Cache exposing (Cache, UserState)
 
 import Debounce exposing (Debounce)
 
