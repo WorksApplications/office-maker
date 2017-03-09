d9908552be2af33c1c6b7ae649b65e36a1645c6f
torii_y <torii_y@worksap.co.jp>
2017-03-09 19:36:04 +0900

    simplify project

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-elm-make src/elm/Page/Map/Main.elm --output=index.js --warn $1 &&
-elm-make src/elm/Page/Login/Main.elm --output=login.js --warn $1 &&
-elm-make src/elm/Page/Master/Main.elm --output=master.js --warn $1 &&
 mkdir -p server/public &&
 mkdir -p server/public/images &&
 mkdir -p server/public/images/floors &&
-cp -f index.js login.js master.js src/style.css server/public
+elm-make src/elm/Page/Map/Main.elm --output=server/public/index.js --warn $1 &&
+elm-make src/elm/Page/Login/Main.elm --output=server/public/login.js --warn $1 &&
+elm-make src/elm/Page/Master/Main.elm --output=server/public/master.js --warn $1 &&
+cp -f src/style.css server/public
