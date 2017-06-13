10a495aaf7c2529a07ec0c1b49c48b4ab669d4c6
seiichi <seiichi19881101@gmail.com>
2017-06-13 20:43:01 +0900

    Delete command

--- server/initialize.sh
+++ server/initialize.sh
@@ -12,7 +12,6 @@ cat defaultConfig.json \
   | sed -e "s/\"user\": \"root\"/\"user\": \"$user\"/"\
   | sed -e "s/\"pass\": \"\"/\"pass\": \"$pass\"/" \
   > config.json
-node commands.js createDataForDebug
 mysql --user=$user --password=$pass -e "drop database map2;"
 mysql --user=$user --password=$pass < sql/1-create-schema.sql
 mysql --user=$user --password=$pass < sql/2-create-tables.sql
