96bb66476642696fb1453e2c6b750dbd797903aa
torii_y <torii_y@worksap.co.jp>
2017-07-03 20:30:50 +0900

    custom authorizer

---
+++ profile-service/functions/authorizer/index.js
@@ -0,0 +1,43 @@
+var jwt = require('jsonwebtoken');
+
+function getSelf(token) {
+  if (!token) {
+    return Promise.reject();
+  }
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, 'mock-key', function(e, user) {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(user);
+      }
+    });
+  }).catch(e => {
+    if (e.name === 'JsonWebTokenError') {
+      return Promise.reject('Unauthorized');
+    } else {
+      return Promise.reject(e.toString());
+    }
+  });
+}
+
+exports.handler = (event, context, callback) => {
+  event.headers = event.headers || {};
+  var token = (event.authorizationToken || '').split('JWT ')[1];
+  if (!token) {
+    callback('Unauthorized');
+    return;
+  };
+  getSelf(token).then(user => {
+    callback(null, {
+      policyDocument: {
+        Statement: [{
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource: event.methodArn
+        }]
+      },
+      context: user
+    });
+  });
+}
