ac7195fd0c19bfabb497160cf234ed1215a16f06
torii_y <torii_y@worksap.co.jp>
2017-07-27 21:52:05 +0900

    add tests, fix permission settings

---
+++ profile-service/test/test.js
@@ -0,0 +1,109 @@
+process.env.EXEC_MODE = 'test';
+
+var childProcess = require('child_process');
+var fs = require('fs');
+var assert = require('assert');
+var request = require('request');
+
+var project = JSON.parse(fs.readFileSync('./project.json', 'utf8'));
+var region = project.region;
+var restApiId = project.restApiId;
+var stageName = 'dev';
+var serviceRoot = `https://${restApiId}.execute-api.${region}.amazonaws.com/${stageName}`;
+var mockAuth = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQURNSU4iLCJ1c2VySWQiOiJtb2NrQGV4YW1wbGUuY29tIiwiaWF0IjoxNDk4NzI2NzAwfQ.H03xsyZJSAdKFsBf6CMCZhnmaUOh9HK0Dn8ty2rimmU';
+
+describe('Profile Service', () => {
+  var dbProcess = null;
+  before(() => {
+    return Promise.resolve();
+  });
+  beforeEach(() => {
+    return Promise.resolve();
+  });
+  describe('GET /profiles/{userId}', () => {
+    it('returns 404 if profile does not exist', () => {
+      var url = serviceRoot + '/profiles/notexist@example.com';
+      return send(mockAuth, 'GET', url).then(assertStatus(404));
+    });
+    it('returns 200 if profile exists', () => {
+      var url = serviceRoot + '/profiles/0001@example.com';
+      return send(mockAuth, 'GET', url).then(res => {
+        if (res.statusCode !== 200) {
+          return Promise.reject('Unexpected statusCode: ' + res.statusCode);
+        }
+        if (!res.body.userId) {
+          return Promise.reject('Unexpected profile: ' + res.body);
+        }
+        return Promise.resolve();
+      });
+    });
+  });
+  describe('PUT /profiles/{userId}', () => {
+    it('returns 400 if body is invalid (userId required)', () => {
+      var url = serviceRoot + '/profiles/0001@example.com';
+      var data = {
+        name: 'Test'
+      };
+      return send(mockAuth, 'PUT', url, data).then(assertStatus(400));
+    });
+    it('returns 400 if body is invalid (name required)', () => {
+      var url = serviceRoot + '/profiles/0001@example.com';
+      var data = {
+        userId: '0001@example.com'
+      };
+      return send(mockAuth, 'PUT', url, data).then(assertStatus(400));
+    });
+    it('returns 200 if everything is ok', () => {
+      var url = serviceRoot + '/profiles/0001@example.com';
+      var data = {
+        "userId": "0001@example.com",
+        "name": "Test"
+      };
+      return send(mockAuth, 'PUT', url, data).then(assertStatus(200));
+    });
+  });
+});
+
+function send(authorization, method, url, data) {
+  return new Promise((resolve, reject) => {
+    var options = {
+      method: method,
+      url: url,
+      headers: {
+        'Authorization': authorization || ''
+      },
+      body: data || undefined,
+      json: true
+    };
+    request(options, (e, response) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+
+
+function assertStatus(expect) {
+  return res => {
+    if (res.statusCode !== expect) {
+      return Promise.reject(`Expected statusCode ${expect} but got ${res.statusCode}: ${JSON.stringify(res)}`);
+    }
+    return Promise.resolve();
+  };
+}
+
+function reducePromises(promises) {
+  return promises.reduce((prev, toPromise) => {
+    return prev.then(toPromise);
+  }, Promise.resolve());
+}
+
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
+}
